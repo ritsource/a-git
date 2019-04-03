c56bac92fae744b3e040689252b137198632cdc3
Ritwik Saha <ritwiksahaofficial@gmail.com>
2019-04-03 09:32:20 +0530

    Added function to recursively find ".git" directory in parent directories

--- src/repository.go
+++ src/repository.go
@@ -8,26 +8,35 @@ import (
 	"fmt"
 	"os"
 	"path"
+	"path/filepath"
 
 	"gopkg.in/ini.v1"
 )
 
-// repository
+// GitRepository - Repository type
+type GitRepository struct {
+	workdir string
+	gitdir  string
+}
 
 // CreateRepository - creates directories and files necessary for a git repository
 func CreateRepository(workdir string) error {
 	var err error
-	// Present Working Directory if "workdir" not specified
-	if workdir == "" {
-		workdir = "."
-	}
+	// // Present Working Directory if "workdir" not specified
+	// if workdir == "" {
+	// 	workdir = "."
+	// }
+
+	gitdir := path.Join(workdir, ".git")
+
+	newRepo := GitRepository{workdir, gitdir}
 
 	// wDir - the specified worktree (directory), by default it's "." (pwd)
-	wDir, err := os.Stat(workdir)
+	wDir, err := os.Stat(newRepo.workdir)
 
 	if os.IsNotExist(err) {
 		// If wDir doesn't exist
-		cErr := os.Mkdir(workdir, 0777)
+		cErr := os.Mkdir(newRepo.workdir, 0777)
 		if cErr != nil {
 			// If we failed to create wDir
 			fmt.Println("Specified directory doesn't exist, Unable to even cretae new one,\n", err)
@@ -44,15 +53,13 @@ func CreateRepository(workdir string) error {
 		}
 	}
 
-	gitdir := ".git" // Git directory name
-
-	os.Mkdir(path.Join(workdir, gitdir), 0777) // Create a ".git" directory
-	err = CreateRepoConf(workdir, gitdir)
-	err = CreateRepoDirs(workdir, gitdir)
+	os.Mkdir(newRepo.gitdir, 0777) // Create a ".git" directory
+	err = CreateRepoConf(newRepo.gitdir)
+	err = CreateRepoDirs(newRepo.gitdir)
 
 	if err != nil {
 		// Remove the whole ".git" folder if some error
-		os.RemoveAll(path.Join(workdir, gitdir))
+		os.RemoveAll(path.Join(newRepo.gitdir))
 		fmt.Println("Failed to initialize repository")
 		fmt.Println(err)
 	}
@@ -61,34 +68,34 @@ func CreateRepository(workdir string) error {
 }
 
 // CreateRepoDirs - creates required git directories and files (inside ".git")
-func CreateRepoDirs(workdir string, gitdir string) error {
+func CreateRepoDirs(gitdir string) error {
 	var err error
 
 	// Created Directories "branches", "into", "refs" ect.
-	err = os.MkdirAll(path.Join(workdir, gitdir, "branches"), 0777)
-	err = os.MkdirAll(path.Join(workdir, gitdir, "info"), 0777)
-	err = os.MkdirAll(path.Join(workdir, gitdir, "objects", "info"), 0777)
-	err = os.MkdirAll(path.Join(workdir, gitdir, "objects", "pack"), 0777)
-	err = os.MkdirAll(path.Join(workdir, gitdir, "refs", "heads"), 0777)
-	err = os.MkdirAll(path.Join(workdir, gitdir, "refs", "tags"), 0777)
+	err = os.MkdirAll(path.Join(gitdir, "branches"), 0777)
+	err = os.MkdirAll(path.Join(gitdir, "info"), 0777)
+	err = os.MkdirAll(path.Join(gitdir, "objects", "info"), 0777)
+	err = os.MkdirAll(path.Join(gitdir, "objects", "pack"), 0777)
+	err = os.MkdirAll(path.Join(gitdir, "refs", "heads"), 0777)
+	err = os.MkdirAll(path.Join(gitdir, "refs", "tags"), 0777)
 
 	// Create a "description" file
-	fd, err := os.Create(path.Join(workdir, gitdir, "description"))
+	fd, err := os.Create(path.Join(gitdir, "description"))
 	_, err = fd.Write([]byte("Unnamed repository; edit this file 'description' to name the repository.\n")) // Default
 
 	// Create a "HEAD" fiel
-	fh, err := os.Create(path.Join(workdir, gitdir, "HEAD"))
+	fh, err := os.Create(path.Join(gitdir, "HEAD"))
 	_, err = fh.Write([]byte("ref: refs/heads/master\n")) // Default
 
 	return err
 }
 
 // CreateRepoConf - creates required "config" file (Microsoft INI type file)
-func CreateRepoConf(workdir string, gitdir string) error {
+func CreateRepoConf(gitdir string) error {
 	var err error
 
 	// Filename of configuration ("config")
-	fname := path.Join(workdir, gitdir, "config")
+	fname := path.Join(gitdir, "config")
 
 	// Create a new config file (but check if it is exist or not)
 	if _, err = os.Stat(fname); os.IsNotExist(err) {
@@ -112,4 +119,38 @@ func CreateRepoConf(workdir string, gitdir string) error {
 	cfg.SaveTo(fname)
 
 	return nil
+}
+
+// FindRepository - Finds repository (".git" file)
+// Sometimes users current directory might not be the repo workdir
+// For those cases this function will find recursively find the .git directory going up on
+// on the directory tree
+func FindRepository(currentdir string) (GitRepository, error) {
+	// Absolute current path
+	absCurrent, err := filepath.Abs(currentdir)
+	if err != nil {
+		return GitRepository{}, err
+	}
+
+	// Absolute git directory path
+	gitdir := filepath.Join(absCurrent, ".git")
+
+	// Check if gitdir exists or not
+	gDir, err := os.Stat(gitdir)
+	if err == nil {
+		isGitDir := gDir.IsDir()
+		if isGitDir {
+			return GitRepository{workdir: currentdir, gitdir: gitdir}, nil
+		}
+	}
+
+	// Relative parent directory
+	absParent := filepath.Join(absCurrent, "..")
+	// Check if root or not
+	if absParent == absCurrent {
+		return GitRepository{}, errors.New("No git directory")
+	}
+
+	// Recursively run for the parent
+	return FindRepository(absParent)
 }
