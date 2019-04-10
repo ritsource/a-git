40989494a681b20b640ebe2e7dc84e4a58329aef
Ritwik Saha <ritwiksahaofficial@gmail.com>
2019-04-10 18:16:02 +0530

    Has come up to an Exhaustion, Also, NASA is gonna release the Pic of first blackhole in a few minutes, SO gonna pause this Project for a while -_-

--- cmd/checkout.go
+++ cmd/checkout.go
@@ -2,7 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"path"
 
+	"github.com/ritwik310/a-git/src"
 	"github.com/spf13/cobra"
 )
 
@@ -17,6 +20,57 @@ var checkoutCmd = &cobra.Command{
 		if err != nil {
 			return err
 		}
+
+		tree, err := src.StageDir(".")
+		if err != nil {
+			return err
+		}
+
+		_, err = tree.Write(".git")
+		if err != nil {
+			return err
+		}
+
+		// Creating a new file to save the current commit
+		// so fucked up, can't make it anymore compateble with git
+
+		// Commit tracker path
+		lastcom := path.Join(".git", "lastcommit")
+		var tracker *os.File
+
+		if _, err := os.Stat(lastcom); os.IsNotExist(err) {
+			tracker, err = os.Create(lastcom)
+		} else {
+			tracker, err = os.Open(lastcom)
+		}
+
+		if err != nil {
+			return err
+		}
+
+		defer tracker.Close()
+
+		var data []byte
+
+		_, err = tracker.Read(data)
+		if err != nil {
+			return err
+		}
+
+		fmt.Println("%+s", data)
+
+		// _, err = tracker.Write([]byte(shaStr))
+		// if err != nil {
+		// 	return err
+		// }
+
+		// commit := src.GitCommit{
+		// 	Tree      []byte,
+		// 	Parent    []byte,
+		// 	Author:    "a-git test author <test@example.com>",
+		// 	Committer: "a-git test commiter <test@example.com>",
+		// 	Message:   mFlag,
+		// }
 
 		fmt.Println(mFlag)
 		return nil
